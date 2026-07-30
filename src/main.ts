e4c40baa59cdd1a7b09c5d915c86500dd368df32
abudaudhossain <abudaudhossainsumon@gmail.com>
2026-07-30 16:23:24 +0600

    update package

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { GlobalExceptionFilter } from './common/filters/global-exception.filter'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
-  const config = app.get(ConfigService);
+  const config = app.get(ConfigService); 
  
 
   app.enableCors({
